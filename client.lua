caf57dd7185d4330a0660fc70ef4b8851bfe884b
SrXavi <71549284+lessxavi@users.noreply.github.com>
2022-05-23 17:14:10 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,23 @@
+local health;
+local thirst;
+local hunger;
+CreateThread(function()
+    while true do 
+        TriggerEvent('esx_status:getStatus', 'health', function(status)
+            health = status.getPercent()
+        end)
+        TriggerEvent('esx_status:getStatus', 'hunger', function(status)
+            hunger = status.getPercent()
+        end)
+        TriggerEvent('esx_status:getStatus', 'thirst', function(lul)
+            thirst = lul.getPercent()
+        end)
+        SendNUIMessage({
+            action = 'update',
+            health = GetEntityHealth(PlayerPedId())-100,
+            hunger = hunger,
+            thirst = thirst
+        })
+        Wait(800)
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+version '1.0.0'
+
+name 'xHud_ui'
+
+original_creator {
+    'AXFW'
+}
+
+contributor {
+    '! Xavi#5456'
+}
+
+description 'Update of AXFW Status HUD'
+
+client_script 'client.lua'
+
+files {
+    'index.html'
+}
+ui_page 'index.html'
